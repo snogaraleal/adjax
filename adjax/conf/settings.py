9a59c1faf88e45801ff99084357099f1d5f04dca
Sebastián Nogara <snogaraleal@gmail.com>
2014-07-12 18:43:58 -0300

    Fix module name settings

--- adjax/conf/settings.py
+++ adjax/conf/settings.py
@@ -10,5 +10,5 @@ def get_setting(name, default):
 TEMPLATE = get_setting('template', 'adjax/interface')
 DATA = get_setting('data', {})
 CONTENT_TYPE = get_setting('content_type', 'application/javascript')
-VIEWS_MODULE_NAME = get_setting('module_name', 'ajax')
-TYPES_MODULE_NAME = get_setting('module_name', 'types')
+VIEWS_MODULE_NAME = get_setting('views_module_name', 'ajax')
+TYPES_MODULE_NAME = get_setting('types_module_name', 'types')
