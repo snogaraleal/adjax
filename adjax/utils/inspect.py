2aa32eff21cb2b81cfb791b38e6800f98a7d1404
Sebastian Nogara <snogaraleal@gmail.com>
2015-08-09 06:10:28 -0300

    Fix argspec providing None as defaults

--- adjax/utils/inspect.py
+++ adjax/utils/inspect.py
@@ -6,11 +6,11 @@ if sys.version_info >= (3, 0):
 
     def get_arg_spec(func):
         argspec = getfullargspec(func)
-        return argspec.args, argspec.defaults, argspec.annotations
+        return argspec.args, argspec.defaults or {}, argspec.annotations
 else:
     def get_arg_spec(func):
         argnames = func.func_code.co_varnames[:func.func_code.co_argcount]
-        return argnames, func.func_defaults, {}
+        return argnames, func.func_defaults or {}, {}
 
 
 def get_full_arg_spec(func):
